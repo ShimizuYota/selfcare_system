a84d6a9f5add48883a42e4ebaa780f37c6e1209c
YotaShimizu <sytytuyao@yahoo.co.jp>
2019-10-23 15:50:02 +0900

    html,css,js更新

--- docs/test_8bit/test1.js
+++ docs/test_8bit/test1.js
@@ -1,7 +1,7 @@
 $(function(){
 	
 	$("#default_select").change(function(){
-		var r = $("option:selected").val();
+		var r = $("#default_select option:selected").val();
 		if(r == 0){
 			$(".font1").css("display","inline");
 			$(".font2").css("display","none");
@@ -14,6 +14,23 @@ $(function(){
 			$(".font3").css("display","inline");
 			$(".font1").css("display","none");
 			$(".font2").css("display","none");
+		}else{}
+	});
+	
+	$("#dark_select").change(function(){
+		var r = $("#dark_select option:selected").val();
+		if(r == 0){
+			$(".font1-1").css("display","inline");
+			$(".font2-1").css("display","none");
+			$(".font3-1").css("display","none");
+		}else if(r == 1){
+			$(".font2-1").css("display","inline");
+			$(".font1-1").css("display","none");
+			$(".font3-1").css("display","none");
+		}else if(r == 2){
+			$(".font3-1").css("display","inline");
+			$(".font1-1").css("display","none");
+			$(".font2-1").css("display","none");
 		}else{}
 	});
 	
